4c15c3c95a9cb99f65e91c1d224d62d1e2d7d8bc
omer.yanay <omeryanay753@gmail.com>
2025-01-18 13:14:22 +0200

    Add hugging face model to analyze news for spy to determine if but or sell

--- app/tradingbot.py
+++ app/tradingbot.py
@@ -5,6 +5,7 @@ from lumibot.traders import Trader
 from datetime import datetime 
 from alpaca_trade_api import REST 
 from timedelta import Timedelta 
+from finbert_model import estimate_sentiment
 from dotenv import load_dotenv
 import os
 
@@ -33,37 +34,62 @@ class MLTrader(Strategy):
         last_price = self.get_last_price(self.symbol)
         quantity = round(cash * self.cash_at_risk / last_price,0)
         return cash, last_price, quantity
-
+    
     def get_dates(self): 
         today = self.get_datetime()
         three_days_prior = today - Timedelta(days=3)
         return today.strftime('%Y-%m-%d'), three_days_prior.strftime('%Y-%m-%d')   
+    
+    def get_sentiment(self): 
+        today, three_days_prior = self.get_dates()
+        news = self.api.get_news(symbol=self.symbol, 
+                                 start=three_days_prior, 
+                                 end=today) 
+        news = [ev.__dict__["_raw"]["headline"] for ev in news]
+        probability, sentiment = estimate_sentiment(news)
+        return probability, sentiment
 
     def on_trading_iteration(self):
         cash, last_price, quantity = self.position_sizing() 
-        
-        if cash > last_price:
-            if self.last_trade == None:
-                order = self.create_order(
-                    self.symbol,
-                    quantity,
-                    "buy",
-                    type="market",
-                    take_profit_price=last_price * 1.05,
-                    stop_loss_price=last_price * .95
-                )
-                self.submit_order(order)
-                self.last_trade = "buy"
+        probability, sentiment = self.get_sentiment()
 
-start_date = datetime(2020,1,1)
+        if cash > last_price: 
+            if sentiment == "positive" and probability > .999: 
+                if self.last_trade == "sell": 
+                    self.sell_all() 
+                order = self.create_order(
+                    self.symbol, 
+                    quantity, 
+                    "buy", 
+                    type="bracket", 
+                    take_profit_price=last_price*1.20, 
+                    stop_loss_price=last_price*.95
+                )
+                self.submit_order(order) 
+                self.last_trade = "buy"
+            elif sentiment == "negative" and probability > .999: 
+                if self.last_trade == "buy": 
+                    self.sell_all() 
+                order = self.create_order(
+                    self.symbol, 
+                    quantity, 
+                    "sell", 
+                    type="bracket", 
+                    take_profit_price=last_price*.8, 
+                    stop_loss_price=last_price*1.05
+                )
+                self.submit_order(order) 
+                self.last_trade = "sell"
+
+start_date = datetime(2022,1,1)
 end_date = datetime(2023,12,31) 
 broker = Alpaca(ALPACA_CREDS) 
 strategy = MLTrader(name='mlstrat', broker=broker, 
                     parameters={"symbol":"SPY", 
-                                "cash_at_risk":.01})
+                                "cash_at_risk":.6})
 strategy.backtest(
     YahooDataBacktesting, 
     start_date, 
     end_date, 
-    parameters={"symbol":"SPY", "cash_at_risk":.01}     
+    parameters={"symbol":"SPY", "cash_at_risk":.6}     
 )
