d48f4ee2c3cef9700147d854d6a9533e5eed2593
Yuval Maliniak <yuvalmali5@gmail.com>
2025-01-21 11:43:21 +0200

    main change to start working on it

--- app/main.py
+++ app/main.py
@@ -4,19 +4,13 @@ app = FastAPI()
 db_connection_str = "mongodb://mongo:27017/"
 db = Database(db_connection_str)
 
-# Example usage
-new_user = {
-    "email": "test@example.com",
-    "balance": 1000.0
-}
-user_id = db.insert_user(new_user)
-print(f"Inserted user with ID: {user_id}")
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
-#
-#
-# @app.get("/hello/{name}")
-# async def say_hello(name: str):
-#     return {"message": f"Hello {name}"}
-#
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
